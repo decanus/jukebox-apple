57a14d8e5abbee16af0a780186c51114254bf80e
Jeremy Stucki <stucki.jeremy@gmail.com>
2016-06-29 22:32:02 +0200

    created base project for the framework

---
+++ framework/Jukebox/Sources/Jukebox.h
@@ -0,0 +1,19 @@
+//
+//  Jukebox.h
+//  Jukebox
+//
+//  Created by Jeremy Stucki on 29/06/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jukebox.
+FOUNDATION_EXPORT double Jukebox_VersionNumber;
+
+//! Project version string for Jukebox.
+FOUNDATION_EXPORT const unsigned char Jukebox_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jukebox/PublicHeader.h>
+
+
